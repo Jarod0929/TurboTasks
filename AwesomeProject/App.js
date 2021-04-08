f39bf44be40c8e679ebed8a0839b80df2f534ab9
Jarod Le <JarodLe@icloud.com>
2021-04-08 17:32:46 -0400

    Got Users to work, put some TODOs up and comments up too

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -31,12 +31,30 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import database from '@react-native-firebase/database';
 
-const Users = database().ref("/Database/Users");
 
 function LogIn({ navigation }) {
-  const [textUserName, changeTextUserName] = useState('');
-  const [textPassword, changeTextPassword] = useState('');
-  const [failed, changefailed] = useState(false);
+  const [textUserName, changeTextUserName] = useState('');//For the Username Field
+  const [textPassword, changeTextPassword] = useState('');//For the Password Field
+  const [failed, changefailed] = useState(false);//Only sets to true when they failed once on account and sets feedback message
+  /*const FirstUsers = database().ref("/Database/Users").push(); //First Account and is structure of how it should look
+  FirstUsers.set({ 
+    Username: "Fruit",
+    Password: "Apple"
+  });*/
+  const samePassword = snapshot => {
+    changefailed(true);
+    changeTextPassword("");
+    if(snapshot.val().Password === textPassword){
+      changeTextUserName("");
+      changefailed(false);
+      navigation.navigate("ProjectList");
+    }
+    database().ref("/Database/Users").orderByChild("Username").equalTo(textUserName).off("child_added", samePassword); 
+  };
+
+  const isAccount = () => { //Checks if there is an account
+    database().ref("/Database/Users").orderByChild("Username").equalTo(textUserName).on("child_added", samePassword);//Only works with on, not once
+  };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -62,7 +80,7 @@ function LogIn({ navigation }) {
         value = {textPassword}
       />
       <TouchableHighlight
-        onPress = {() => console.log("Hello World")}
+        onPress = {isAccount}
       >
         <View
           style = {styles.buttonLogIn}
@@ -80,9 +98,35 @@ function LogIn({ navigation }) {
 }
 
 function CreateAccount({ navigation }) {
-  const [textUserName, changeTextUserName] = useState('');
-  const [textPassword, changeTextPassword] = useState('');
-  const [failed, changefailed] = useState(false);
+  const [textUserName, changeTextUserName] = useState('');//For the Username Field
+  const [textPassword, changeTextPassword] = useState('');//For the Password Field
+  const [failed, changefailed] = useState(false);//Only sets to true when they failed once on account and sets feedback message
+  const [succeed, changeSucceed] = useState(false);//Sets to true when succeed after passing by all names
+
+  const anyPreviousUsernames = snapshot => {
+    changeSucceed(false);
+    changefailed(true);
+    database().ref("/Database/Users").orderByChild("Username").equalTo(textUserName).off("child_added", anyPreviousUsernames); 
+  };
+  
+  const createNewAccount = () => {
+    //TODO: Find out way so Users can not spam and by pass multiple Usernames
+    //Tried: Async and Promises shallowly
+    //TODO: Also for some Reason you have to press the button twice
+    changeSucceed(true);
+    database().ref("/Database/Users").orderByChild("Username").equalTo(textUserName).on("child_added", anyPreviousUsernames);//Only works with on, not once
+
+    if(succeed){
+      changeSucceed(false);
+      changefailed(false);
+      database().ref("/Database/Users").push({
+        Username: textUserName,
+        Password: textPassword
+      });
+      navigation.navigate("LogIn");
+    }
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <TouchableHighlight
@@ -107,7 +151,7 @@ function CreateAccount({ navigation }) {
         value = {textPassword}
       />
       <TouchableHighlight
-        onPress = {() => console.log("Hello World")}
+        onPress = {createNewAccount}
       >
         <View
           style = {styles.buttonLogIn}
@@ -124,6 +168,14 @@ function CreateAccount({ navigation }) {
   );
 }
 
+function ProjectList ({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
@@ -132,6 +184,7 @@ export default function App() {
       <Drawer.Navigator initialRouteName="LogIn">
         <Drawer.Screen name="LogIn" component={LogIn} />
         <Drawer.Screen name="CreateAccount" component={CreateAccount} />
+        <Drawer.Screen name="ProjectList" component={ProjectList}/>
       </Drawer.Navigator>
     </NavigationContainer>
   );
