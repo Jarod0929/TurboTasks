d7143bd726bcdf06d7e2495eb6fe0f9c9a352b41
Jarod Le <JarodLe@icloud.com>
2021-04-25 17:29:51 -0400

    Comments on LogIn and CreateAccount

--- AwesomeProject/components/CreateAccount.js
+++ AwesomeProject/components/CreateAccount.js
@@ -8,6 +8,12 @@ import {
 import database from '@react-native-firebase/database';
 import * as styles from './styles.js';
 
+/**
+ * Creates the drawer with all the navigation
+ * 
+ * @param {object} props including navigation and children
+ * @returns the bar under the topbar with navigation to LogIn
+ */
 const Drawer = (props)=>{
   const [drawer, changeDrawer] = useState(false);
   return(
@@ -29,6 +35,12 @@ const Drawer = (props)=>{
   );
 }
 
+/**
+ * Establishes the entire container with all the children under the bar
+ * 
+ * @param {tag} {children} The rest of the tags of CreateAccount
+ * @returns Top blue bar with all its children below it
+ */
 const TopBar = ({children}) => {
   return (
     <View style = {styles.container}>
@@ -39,13 +51,27 @@ const TopBar = ({children}) => {
   )
 };
 
-
+/**
+ * A page where the user creates their account.
+ * 2 input boxes for username and password
+ * 2 buttons for going back to previous screen and creating account
+ * Once account has been created, user is forced to LogIn screen
+ * 
+ * @param {object} navigation For the user to move between screens 
+ * @returns {tag} The page of CreateAccount
+ */
 export function CreateAccount ({navigation}) {
   const [username, changeUsername] = useState('');//For the Username Field
   const [password, changePassword] = useState('');//For the Password Field
   const [failed, changeFailed] = useState(false);//Only sets to true when they failed once on account and sets feedback message
   
-  const anyPreviousUsernames = snapshot => {
+  /**
+   * If no users are found snapshot will be null and the account will be created
+   * If a specific user is found, snapshot will be their account and feedback will be given
+   * 
+   * @param {object} snapshot Is the object of a specific user or is null
+   */
+  const isUniqueUsername = snapshot => {
     if(snapshot.val() !== null || username === '' || password === ''){
       changeFailed(true);
     } else {
@@ -64,15 +90,24 @@ export function CreateAccount ({navigation}) {
       changeFailed(false);
       navigation.navigate("LogIn");
     }
-    database().ref("/Database/Users").orderByChild("Username").equalTo(username).off("value", anyPreviousUsernames); 
-  };
-    
-  const createNewAccount = () => {
-    database().ref("/Database/Users").orderByChild("Username").equalTo(username).on("value", anyPreviousUsernames);
+    database().ref("/Database/Users").orderByChild("Username").equalTo(username).off("value", isUniqueUsername); 
   };
   
-  const goToLogIn = () => { //Goes to CreateAccount Screen
-    changeUsername(""); //Resets all changes made
+  /**
+   * Access the database to find all users that have the same username
+   * Will create account if no users are found
+   * Always calls isUniqueUsername()
+   */
+  const createNewAccount = () => {
+    database().ref("/Database/Users").orderByChild("Username").equalTo(username).on("value", isUniqueUsername);
+  };
+  
+  /**
+   * Clears all inputs of username, passord, and failed.
+   * Navigates user to LogIn screen.
+   */
+  const goToLogIn = () => { 
+    changeUsername(""); 
     changePassword("");
     changeFailed(false);
     navigation.navigate("LogIn");
