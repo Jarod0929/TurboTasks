7ef36c6a867320bd422f65df471021c8b20a8e7d
Cameron <czerrilla@mail.roanoke.edu>
2021-04-27 17:54:47 -0400

    update

--- AwesomeProject/components/Settings.js
+++ AwesomeProject/components/Settings.js
@@ -78,10 +78,13 @@ const TopBar = (props) => {
 
 export function Settings({ route, navigation }) {
   const [userEnteredPassword,changeUserEnteredPassword]=useState(''); // user entered password
+  const [userEnteredPasswordForUsername,changeUserEnteredPasswordForUsername]=useState(''); // this takes a password first then a new username second
   const [password,changePassword]=useState(''); // pulled database password
   const [username,changeUser]=useState(''); // username
-  const [inputText,changeinputText]=useState('Password'); // input text for background of box
+  const [inputTextForPassword,changeinputTextForPassword]=useState('Password'); // input text for background of box
+  const [inputTextForUsername,changeinputTextForUsername]=useState('Password');
   const [Accepted,changeAccepted]=useState("False"); // password is true or false 
+  const [usernameChangeValid,changeUsernameValidity]=useState("False"); // for changing the username
 
   //finds the username of the current logged in user
   const handleUsername = snapshot => {
@@ -90,9 +93,12 @@ export function Settings({ route, navigation }) {
   //finds the password of the current logged in user
   const handlePassword = snapshot => {
       changePassword(snapshot.val());
+     
   }
+  
   //test to see if the typed in password is the correct one
   const isPassword = () =>{
+  
     //If the user entered the correct password and they have typed in a new password and hit enter
     // then their password will be changed
     if(Accepted=='True'){
@@ -100,11 +106,12 @@ export function Settings({ route, navigation }) {
         database().ref("/Database/Users/" + route.params.user).update({Password: userEnteredPassword});
         changeUserEnteredPassword('');
         changeAccepted('False');
-        changeinputText('Enter your password');
-        database().ref("/Database/Users/" + route.params.user+"/Password").once("value", handlePassword);
+        changeinputTextForPassword('Enter your password');
+        database().ref("/Database/Users/" + route.params.user+"/Password").on("value", handlePassword);
+      
       } else {
-        changeUserEnteredPassword('');
-        changeinputText("Something Went Wrong");
+        changeAccepted('False');
+        changeinputTextForPassword("Something Went Wrong");
       }    
 
     // If it its the correct password the user will enter in their new desired password
@@ -112,9 +119,33 @@ export function Settings({ route, navigation }) {
       console.log("right");
       changeUserEnteredPassword('');
       changeAccepted('True');
-      changeinputText("Enter New Password");
+      changeinputTextForPassword("Enter New Password");
     } else {
       console.log("wrong Password Try again");
+      
+      
+    }
+  }
+  const changeUsername=()=>{
+    if(userEnteredPasswordForUsername==password){
+      changeUserEnteredPasswordForUsername('');
+      changeUsernameValidity('True');
+      changeinputTextForUsername("Enter New Username");
+    }
+    else if(usernameChangeValid=='True'){
+      if(userEnteredPassword != ''){
+        database().ref("/Database/Users/" + route.params.user).update({Username: userEnteredPasswordForUsername});
+        changeUserEnteredPassword('');
+        changeUsernameValidity('False');
+        changeinputTextForUsername('Enter your password');
+        database().ref("/Database/Users/" + route.params.user+"/Username").once("value", handleUsername);
+      } else {
+        changeUserEnteredPassword('');
+        changeinputTextForUsername("Something Went Wrong");
+      }   
+    }
+    else{
+      console.log("wrong password try again");
     }
   }
   
@@ -133,11 +164,27 @@ export function Settings({ route, navigation }) {
       <View style={styles.settingsPage}>
         <View style = {styles.flexAlignContainer}>
           <Text>Hello {username} Welcome to your Settings</Text> 
+          <Text>Enter your password to Change Your Username</Text>
+          <View style={styles.textInputContainer}>
+            <TextInput 
+              onChangeText = {text => changeUserEnteredPasswordForUsername(text)}
+              placeholder = {inputTextForUsername}
+              value = {userEnteredPasswordForUsername}
+            />
+          </View>
+          <TouchableHighlight 
+            style={styles.buttonContainer} 
+            onPress={()=>
+              changeUsername()
+            }
+          >
+            <Text style={styles.buttonText}>Enter</Text>
+          </TouchableHighlight>
           <Text>Enter your password to Change Your password</Text>
           <View style={styles.textInputContainer}>
             <TextInput 
               onChangeText = {text => changeUserEnteredPassword(text)}
-              placeholder = {inputText}
+              placeholder = {inputTextForPassword}
               value = {userEnteredPassword}
             />
           </View>
